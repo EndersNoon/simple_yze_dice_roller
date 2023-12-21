503466dc990bb24eb492d4f489b498abecb11147
EndersNoon <enders.noon@gmail.com>
2023-12-21 17:35:47 +0100

    create separate modules

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
 
+  const dice_area = document.getElementById("dice_area");
+  console.log(dice_area);
+
   const new_dice = new DiceType();
   console.log(new_dice.name);
 
@@ -13,10 +16,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
   second_dice.log_to_console();
   second_dice.set_value(4);
   second_dice.log_to_console();
+
+  let new_dice_holder = new DiceHolder(new_dice);
+  new_dice_holder.add_dice();
+  new_dice_holder.get_dice_num_info();
+
+  new_dice_holder.add_dice();
+  new_dice_holder.add_dice();
+  new_dice_holder.add_dice();
+  new_dice_holder.get_dice_num_info();
+
+  render_dice(dice_area, new_dice_holder);
 });
 
 class DiceType {
-  constructor(name = "Default dice", color = "white", reroll_status = true) {
+  constructor(name = "Default", color = "white", reroll_status = true) {
     this.name = name;
     this.color = color;
     this.reroll_status = reroll_status;
@@ -30,4 +44,43 @@ class DiceType {
   set_value(value) {
     this.value = value;
   }
+}
+
+class DiceHolder {
+  constructor(dice_type) {
+    this.dice_type = dice_type;
+    this.dice_list = [];
+  }
+
+  add_dice() {
+    this.dice_list.push(this.dice_type);
+  }
+
+  get_dice_num_info() {
+    console.log(
+      "There is " +
+        this.dice_list.length +
+        " " +
+        this.dice_type.name +
+        " dice stored",
+    );
+  }
+  get_num_of_dice() {
+    return this.dice_list.length;
+  }
+  get_dice_array() {
+    return this.dice_list;
+  }
+}
+
+function render_dice(dice_area, dice_holder) {
+  let dice_array = dice_holder.get_dice_array();
+  dice_array.forEach((element) => {
+    const new_dice_div = document.createElement("div");
+    new_dice_div.classList.add("dice", "first-face");
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+    new_dice_div.appendChild(dot);
+    dice_area.appendChild(new_dice_div);
+  });
 }
